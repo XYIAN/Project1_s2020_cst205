a4415cc35884b9fda3645441dde68ee34f33ebf6
XYIAN <kylexyiandilbeck@gmail.com>
2020-04-06 18:05:27 -0700

    skeleton

---
+++ main.py
@@ -0,0 +1,13 @@
+# @Author   KXD, , ,
+# @Title    CST205 | Group Project 1
+# @Abstract main python page to load flask templates to preform operations on given api and user data
+# @Date     4/6/2020
+# @v1.0.0
+
+#begin imports 
+
+#end imports 
+
+
+
+#END main.py 
