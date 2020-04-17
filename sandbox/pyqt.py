ec1626728b1512b59f7bce655030d7ddbbdc7131
XYIAN <kylexyiandilbeck@gmail.com>
2020-04-16 18:40:32 -0700

    sandbox add

---
+++ sandbox/pyqt.py
@@ -0,0 +1,20 @@
+#project 11 pyqt snadbox 
+
+#imports
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QComboBox 
+from PyQt5.QtCore import pyqtSlot
+form PyQt5.QtGui import QIcon, QPixmap
+#end imports 
+
+#Hey gang, Kyle here! 
+#This is a "sandbox" aka area to play around and mess with code that wont affect our outter files! :)
+#Let me know via slack if you have any questions about code or git! :) happy coding! 
+
+#begin code
+class Sandbox(QWidget):
+    #constructor definition 
+    def __init__(self):
+
+
+#end gui class 
